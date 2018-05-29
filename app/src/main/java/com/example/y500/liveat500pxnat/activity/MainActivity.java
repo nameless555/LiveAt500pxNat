367b97485a9724c21e0bd272c15750d2b03b43a9
nameless555 <nameless_classic@live.com>
2018-05-29 16:41:53 +0700

    test

--- app/src/main/java/com/example/y500/liveat500pxnat/activity/MainActivity.java
+++ app/src/main/java/com/example/y500/liveat500pxnat/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.y500.liveat500pxnat;
+package com.example.y500.liveat500pxnat.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.y500.liveat500pxnat.R;
 
 public class MainActivity extends AppCompatActivity {
 
