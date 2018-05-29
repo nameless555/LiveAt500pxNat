818bf4b78561620395291ce5893fa44d624c3ddc
nameless555 <nameless_classic@live.com>
2018-05-29 18:44:14 +0700

    test

---
+++ app/src/main/java/com/example/y500/liveat500pxnat/MainApplication.java
@@ -0,0 +1,21 @@
+package com.example.y500.liveat500pxnat;
+
+import android.app.Application;
+
+import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Contextor.getInstance().init(getApplicationContext());
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+}
